cedbae66ea5da165a7b80db19858ddbe7aea50ef
vinh.nguyenquang <vinhnq.nguyenquang@vndirect.com.vn>
2018-06-08 17:57:26 +0700

    PE-002: Even-Fibonacci-numbers

---
+++ PE-002/bruteforce_pe002.py
@@ -0,0 +1,22 @@
+def sum_fibonacci_under_4_million():
+    fi_1st = 1
+    fi_2nd = 2
+    sum_even = fi_2nd
+    fi_n = fi_1st + fi_2nd
+    while fi_n < 4000000:
+        if not fi_n % 2:
+            sum_even += fi_n
+        fi_1st, fi_2nd = fi_2nd, fi_n
+        fi_n = fi_1st + fi_2nd
+    return sum_even
+
+
+if __name__ == "__main__":
+    import time
+    print("bruteforce fibonacci result:")
+    start = time.time()
+    print(sum_fibonacci_under_4_million())
+    done = time.time()
+    elapsed = done - start
+    print("elapsed time: {}s".format(elapsed))
+
---
+++ PE-002/math_pe002.py
@@ -0,0 +1,21 @@
+def sum_fibonacci_under_4_million():
+    fi_n_6 = 2
+    fi_n_3 = 8
+    sum_even = fi_n_3 + fi_n_6
+    fi_n = 4 * fi_n_3 + fi_n_6
+    while fi_n < 4000000:
+        sum_even += fi_n
+        fi_n_3, fi_n_6 = fi_n, fi_n_3
+        fi_n = 4 * fi_n_3 + fi_n_6
+    return sum_even
+
+
+if __name__ == "__main__":
+    import time
+    print("math fibonacci result:")
+    start = time.time()
+    print(sum_fibonacci_under_4_million())
+    done = time.time()
+    elapsed = done - start
+    print("elapsed time: {}s".format(elapsed))
+
