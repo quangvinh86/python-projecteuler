bfedc654b14d23f96835762d3f38f64051c8e3db
vinh.nguyenquang <vinhnq.nguyenquang@vndirect.com.vn>
2018-06-11 14:11:45 +0700

    Problem 9: Pythagorean-triplets

--- PE-009/pythagorean_triplets.py
+++ PE-009/pythagorean_triplets.py
@@ -1,3 +1,12 @@
+"""
+Solution to Project Euler problem 9
+@author: vinh.nguyenquang
+@email: quangvinh19862003@gmail.com
+
+
+"""
+
+
 def find_pythagorean_triplets(sum_triple):
     for a in range(1, int(sum_triple // 3)):
         for b in range(a, int(sum_triple // 2)):
